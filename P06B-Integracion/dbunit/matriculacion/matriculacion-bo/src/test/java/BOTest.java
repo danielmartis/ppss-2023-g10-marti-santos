35ec592b76e2e605e1b49386fe5312bcd5f64d4a
daniel <47526269+danielmartis@users.noreply.github.com>
2024-08-29 20:59:30 +0200

    Delete P06B-Integracion/dbunit directory

--- P06B-Integracion/dbunit/matriculacion/matriculacion-bo/src/test/java/BOTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class BOTest {
-    @Test
-    public void test() {
-        Assertions.assertTrue(true);
-    }
-}
